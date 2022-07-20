5c0febe3b22faa1ac031f0da22e5a64012d0f90b
HashimAlSada <HashimAlSadah2894@gmail.com>
2022-07-20 18:20:05 +0300

    upload project

---
+++ main.js
@@ -0,0 +1,27 @@
+const emailInputIntro = document.querySelector(".intro .email-form input");
+const emailInputEarlyAccess = document.querySelector(".early-access .email-form input");
+const buttonIntro = document.querySelector(".intro .email-form button");
+const buttonEarlyAccess = document.querySelector(".early-access .email-form button");
+const emailRegExp = new RegExp("(^[a-z0-9])([a-z0-9\!\#\$\%\&\'\*\+\-\/\=\?\^\_\`\{\|\]\*]*)@[a-z0-9]+[\.]{1}.+[a-z0-9]+$", "i");
+const introErrorMessage = document.querySelector(".intro .error-message");
+const earlyAccessErrorMessage = document.querySelector(".early-access .error-message");
+buttonIntro.addEventListener("click", (event)=>{
+    event.preventDefault();
+    const email = emailInputIntro.value;
+    if(!emailRegExp.test(email)){
+        introErrorMessage.classList.add("active");
+    }else if(emailRegExp.test(email)){
+        introErrorMessage.classList.remove("active");
+    }
+})
+
+buttonEarlyAccess.addEventListener("click", (event)=>{
+    event.preventDefault();
+    const email = emailInputEarlyAccess.value;
+    if(!emailRegExp.test(email)){
+        earlyAccessErrorMessage.classList.add("active");
+    }else if(emailRegExp.test(email)){
+        earlyAccessErrorMessage.classList.remove("active");
+    }
+})
+
